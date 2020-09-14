b03a658caca7cc29b797d7b0f5e379aa1c5c2a20
ganesh.takale <ganesh.takale@perennialsys.com>
2020-09-14 23:42:26 +0530

    first commit

---
+++ springboot-c3p0-connectionleak/src/test/java/com/ganeshtakale/springboot/c3p0/connectionleak/SpringbootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshtakale.springboot.c3p0.connectionleak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
